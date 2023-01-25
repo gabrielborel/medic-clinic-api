eaa55e955eab35bd28311ee0d41eca4f3bf75a9c
Gabriel Borel <biel_borel@hotmail.com>
2023-01-25 09:24:03 -0300

    medic controller created

---
+++ src/main/java/medic/clinic/api/controller/MedicController.java
@@ -0,0 +1,12 @@
+package medic.clinic.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/medics")
+public class MedicController {
+
+}
