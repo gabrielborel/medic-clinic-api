7997681230b6eb21957316c7a9fb9543ed60abc9
Gabriel Borel <biel_borel@hotmail.com>
2023-01-25 10:50:00 -0300

    create table medic migration added

---
+++ src/main/resources/db/migration/V1__create-table-medics.sql
@@ -0,0 +1,16 @@
+CREATE TABLE medics
+(
+    id         BIGINT  NOT NULL AUTO_INCREMENT,
+    name       VARCHAR(100) NOT NULL,
+    email      VARCHAR(100) NOT NULL UNIQUE,
+    crm        VARCHAR(6) NOT NULL UNIQUE,
+    specialty  VARCHAR(100) NOT NULL,
+    street     VARCHAR(100) NOT NULL,
+    district   VARCHAR(100) NOT NULL,
+    code       VARCHAR(9) NOT NULL,
+    complement VARCHAR(100),
+    number     VARCHAR(20),
+    state      CHAR(2) NOT NULL,
+    city       VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
