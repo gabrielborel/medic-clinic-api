765923334838be08d9924e284f7b8da91cfa3188
Gabriel Borel <biel_borel@hotmail.com>
2023-01-25 12:07:34 -0300

    pagination and sort added to list medics route

--- src/main/java/medic/clinic/api/controller/MedicController.java
+++ src/main/java/medic/clinic/api/controller/MedicController.java
@@ -6,10 +6,10 @@ import medic.clinic.api.medic.ListMedicDTO;
 import medic.clinic.api.medic.Medic;
 import medic.clinic.api.medic.MedicRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/medics")
@@ -24,11 +24,9 @@ public class MedicController {
    }
 
    @GetMapping
-   public List<ListMedicDTO> list() {
+   public Page<ListMedicDTO> list(@PageableDefault(size = 10) Pageable pagination) {
       return repository
-         .findAll()
-         .stream()
-         .map(ListMedicDTO::new)
-         .toList();
+         .findAll(pagination)
+         .map(ListMedicDTO::new);
    }
 }
