3367550befe27160b2bdf31d08fd05dbc98d4ed3
Gabriel Borel <biel_borel@hotmail.com>
2023-01-25 16:36:14 -0300

    find all medic by active true method added to repo

--- src/main/java/medic/clinic/api/medic/MedicRepository.java
+++ src/main/java/medic/clinic/api/medic/MedicRepository.java
@@ -1,6 +1,9 @@
 package medic.clinic.api.medic;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MedicRepository extends JpaRepository<Medic, Long> {
+   Page<Medic> findAllByActiveTrue(Pageable pagination);
 }
