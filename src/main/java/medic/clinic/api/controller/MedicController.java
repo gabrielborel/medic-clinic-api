12e5e9903db5e86a668e406a7626bc82261b1ba2
Gabriel Borel <biel_borel@hotmail.com>
2023-01-25 10:50:17 -0300

    added transactional decorator to create medic route

--- src/main/java/medic/clinic/api/controller/MedicController.java
+++ src/main/java/medic/clinic/api/controller/MedicController.java
@@ -4,6 +4,7 @@ import medic.clinic.api.medic.CreateMedicDTO;
 import medic.clinic.api.medic.Medic;
 import medic.clinic.api.medic.MedicRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ public class MedicController {
    private MedicRepository repository;
 
    @PostMapping
+   @Transactional
    public void create(@RequestBody CreateMedicDTO data) {
       repository.save(new Medic(data));
    }
