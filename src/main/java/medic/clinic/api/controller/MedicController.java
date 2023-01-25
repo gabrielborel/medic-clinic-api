afb7a92dc8f76ffdd6279532ec736ae8b553299f
Gabriel Borel <biel_borel@hotmail.com>
2023-01-25 10:22:25 -0300

    medic repo injected and create medic post route added

--- src/main/java/medic/clinic/api/controller/MedicController.java
+++ src/main/java/medic/clinic/api/controller/MedicController.java
@@ -1,5 +1,9 @@
 package medic.clinic.api.controller;
 
+import medic.clinic.api.medic.CreateMedicDTO;
+import medic.clinic.api.medic.Medic;
+import medic.clinic.api.medic.MedicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,5 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/medics")
 public class MedicController {
+   @Autowired
+   private MedicRepository repository;
 
+   @PostMapping
+   public void create(@RequestBody CreateMedicDTO data) {
+      repository.save(new Medic(data));
+   }
 }
